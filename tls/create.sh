91391e8ca87b6e1e6bfbf9ed14e5ccd64419e77e
Jordan Jennings <JJN2009@gmail.com>
2016-04-06 00:48:05 -0700

    update cert creation to use new vm name scheme

--- tls/create.sh
+++ tls/create.sh
@@ -7,8 +7,7 @@ cfssl gencert -initca ca-csr.json | cfssljson -bare certs/ca -
 cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=server server.json | cfssljson -bare certs/server
 
 #Generate Client-Server
-cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client-server core-01.json | cfssljson -bare certs/core-01
+cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client-server core-master-01.json | cfssljson -bare certs/core-master-01
 
 #Generate Client
 cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare certs/client
-
