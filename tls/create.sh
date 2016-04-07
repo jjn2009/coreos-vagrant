6d9c21c97a0000fbb988adb1b510b8de495e7907
Jordan Jennings <JJN2009@gmail.com>
2016-04-06 20:26:33 -0700

    change permission of create script, allow arguments for number of worker and master

--- tls/create.sh
+++ tls/create.sh
@@ -1,3 +1,8 @@
+if [ -z "$1" ] ; do
+  echo "Usage: ./create.sh NUM_MASTER [NUM_WORKER]"
+  exit 1
+done
+
 #Create CA
 cfssl gencert -initca ca-csr.json | cfssljson -bare certs/ca -
 
@@ -5,7 +10,21 @@ cfssl gencert -initca ca-csr.json | cfssljson -bare certs/ca -
 cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=server server.json | cfssljson -bare certs/server
 
 #Generate Client-Server
-cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client-server core-master.json | cfssljson -bare certs/core-master-01
+for i in `seq $1`; do
+  cfssl gencert \-ca=certs/ca.pem -ca-key=certs/ca-key.pem \
+      -config=ca-config.json -profile=client-server core-master.json \
+      | cfssljson -bare certs/core-master-`printf "%02d" $i`
+done
+
+#Generate Client-Server
+if [ ! -z $2 ]; do
+  for i in `seq $2`; do
+    cfssl gencert \-ca=certs/ca.pem -ca-key=certs/ca-key.pem \
+        -config=ca-config.json -profile=client-server core-master.json \
+        | cfssljson -bare certs/core-master-`printf "%02d" $i`
+  done
+done
+
 
 #Generate Client
 cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare certs/client
