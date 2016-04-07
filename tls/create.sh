c81aa77672ef56019694b2a62f17a55edbdd46c5
Jordan Jennings <JJN2009@gmail.com>
2016-04-06 17:42:18 -0700

    use more generic name for config

--- tls/create.sh
+++ tls/create.sh
@@ -1,13 +1,11 @@
-
 #Create CA
 cfssl gencert -initca ca-csr.json | cfssljson -bare certs/ca -
-
 
 #Generate Server
 cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=server server.json | cfssljson -bare certs/server
 
 #Generate Client-Server
-cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client-server core-master-01.json | cfssljson -bare certs/core-master-01
+cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client-server core-master.json | cfssljson -bare certs/core-master-01
 
 #Generate Client
 cfssl gencert -ca=certs/ca.pem -ca-key=certs/ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare certs/client
